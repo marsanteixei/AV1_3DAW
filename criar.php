f361fb2b1187b81ed99f859bfb315966026f6e98
Márcio dos Santos Teixeira <65796964+marsanteixei@users.noreply.github.com>
2021-05-21 01:50:44 -0300

    Update criar.php

--- criar.php
+++ criar.php
@@ -5,11 +5,11 @@
 	  $user = "root";
 	  $password = "";
 	  $dataBaseName="av1";
-      $conn = mysqli_connect($server,$user,$password,$dataBaseName) or die("connection error: " . $conn->connect_error);
+          $conn = mysqli_connect($server,$user,$password,$dataBaseName) or die("connection error: " . $conn->connect_error);
 	  $codigo =$_POST["codigo"];
-      $nome =$_POST["nome"];
+          $nome =$_POST["nome"];
 	  $periodo =$_POST["periodo"];
-      $credito =$_POST["credito"];
+          $credito =$_POST["credito"];
 	  $resposta =$_POST["resposta"];
 	  $query = mysqli_query($conn,"INSERT INTO disciplina (codigo, nome, periodo, credito) VALUES ('$codigo', '$nome', '$periodo', '$credito')");
 	  if($resposta == 'sim'){
